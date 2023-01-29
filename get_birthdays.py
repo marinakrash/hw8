f281516929b2c56ca762ea93d2e8ed0c5b620b55
Marinakrash <66419111+marinakrash@users.noreply.github.com>
2023-01-29 23:17:44 +0200

    Add files via upload

---
+++ get_birthdays.py
@@ -0,0 +1,36 @@
+import datetime
+
+from collections import defaultdict, OrderedDict
+
+users=[{'name':'Kell', 'birthday':datetime.date(2005, 2, 2)}, {'name':'Kely', 'birthday':datetime.date(2001, 1, 5)},
+       {'name':'Kety', 'birthday':datetime.date(2000, 2, 2)},{'name':'Kem', 'birthday':datetime.date(1999, 2, 4)}]
+
+def get_birthdays_per_week(users):
+    birth_list=[]
+    grouped_birthday = defaultdict(list)
+    current_date = (datetime.datetime.now()).date()
+    weeks_interval = datetime.timedelta(weeks=1)
+    future_week = current_date + weeks_interval
+    for i in users:
+        birthday=(i['birthday'])
+        birthday_new=datetime.date(current_date.year, birthday.month, birthday.day)
+        date_list = [current_date + datetime.timedelta(days=x) for x in range(7)]
+        if birthday_new in date_list:
+            birth_list.append([i['name'], birthday_new.strftime('%A')])
+    for name, date in birth_list:
+        if date == 'Sunday' or date == 'Saturday':
+            date='Monday'
+        grouped_birthday[date].append(name)
+    week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+    result = OrderedDict()
+    for day in week:
+        if day in grouped_birthday:
+            result[day] = grouped_birthday[day]
+    print(result)
+    return(result)
+
+
+
+
+
+
